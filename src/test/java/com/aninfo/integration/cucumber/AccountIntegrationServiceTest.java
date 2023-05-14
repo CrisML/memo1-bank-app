d8b01c6fa768e5c20a6b78d7a5ccf742f646bcf7
CrisML <clin@fi.uba.ar>
2023-05-14 00:42:10 -0300

    Modificacion de AccountIntegrationServiceTest para que se utilizen los metodos de transacciones y no del account directamente. Tambien se separaron los test de cucumber para que no tire error al ejecutarlo

--- src/test/java/com/aninfo/integration/cucumber/AccountIntegrationServiceTest.java
+++ src/test/java/com/aninfo/integration/cucumber/AccountIntegrationServiceTest.java
@@ -2,10 +2,14 @@ package com.aninfo.integration.cucumber;
 
 import com.aninfo.Memo1BankApp;
 import com.aninfo.model.Account;
+import com.aninfo.model.Transaction;
 import com.aninfo.service.AccountService;
+import com.aninfo.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Optional;
 
 @ContextConfiguration(classes = Memo1BankApp.class)
 @WebAppConfiguration
@@ -14,16 +18,33 @@ public class AccountIntegrationServiceTest {
     @Autowired
     AccountService accountService;
 
+    @Autowired
+    TransactionService transactionService;
+
     Account createAccount(Double balance) {
         return accountService.createAccount(new Account(balance));
     }
 
     Account withdraw(Account account, Double sum) {
-        return accountService.withdraw(account.getCbu(), sum);
+        Transaction transaction = new Transaction();
+        transaction.setCbu(account.getCbu());
+        transaction.setAmount(sum);
+        transaction.setCategory("withdraw");
+        transactionService.createTransaction(transaction);
+
+        Optional<Account> newAccount = accountService.findById(account.getCbu());
+        return newAccount.get();
     }
 
     Account deposit(Account account, Double sum) {
-        return accountService.deposit(account.getCbu(), sum);
+        Transaction transaction = new Transaction();
+        transaction.setCbu(account.getCbu());
+        transaction.setAmount(sum);
+        transaction.setCategory("deposit");
+        transactionService.createTransaction(transaction);
+
+        Optional<Account> newAccount = accountService.findById(account.getCbu());
+        return newAccount.get();
     }
 
 }
